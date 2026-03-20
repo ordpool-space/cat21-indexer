530f40c50e02cb8ebf2c54a852084cafc217d9f0
Hans Crypto <johannes@haushoppe.art>
2026-03-20 20:20:10 +0100

    temp: use koyeb domain until backend.cat21.space DNS is resolved

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'https://backend.cat21.space',
+  api: 'https://cat21-backend.koyeb.app',
   ordpoolExplorer: 'https://ordpool.space',
   ordExplorer: 'https://ord.cat21.space',
 };
